eaa312d2ce8f19c3f98cc12c3653ff1bd2bec4e3
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2020-08-31 08:22:28 -0300

    Create Exercise #077

---
+++ #077 - Contando vogais em Tupla.py
@@ -0,0 +1,8 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
+for palavra in palavras:
+    print(f'Na palavra {palavra.upper()} temos ', end='')
+    for letra in palavra:
+        if letra in 'AaEeIiOoUu':
+            print(letra, end=' ')
+    print('')
