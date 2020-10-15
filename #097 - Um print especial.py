5116ebca1f97f010815302d4747ac8b3747af10f
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2020-10-14 21:18:08 -0300

    Create #097 - Um print especial.py

---
+++ #097 - Um print especial.py
@@ -0,0 +1,13 @@
+def escreva(txt):
+    tam = (len(txt) + 4)
+    print('~' * tam)
+    #print(' ',txt,' ')
+    print(f'  {txt}')
+    print('~' * tam)
+
+
+escreva('Olá, mundo!')
+escreva('Eu sou o Lucas :)')
+escreva('Estou aprendendo Python')
+escreva('Com o Professor Guanabara')
+escreva('No CursoEmVideo')
